c01f38e54536ba77b991d9dd0a917129f00612e8
e-business <e-business@live.nl>
2026-03-18 16:36:25 +0000

    File changes

--- base44/functions/sendWhatsAppMessage/entry.ts
+++ base44/functions/sendWhatsAppMessage/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.21';
 
 function normalizeNlPhone(input: string): string {
   let p = (input || '').replace(/\D/g, '');
